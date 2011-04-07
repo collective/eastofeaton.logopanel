98541f8cb2c88c2ac8243e77668841f7108481c9
cewing <cris@crisewing.com>
2011-04-06 17:36:13 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+.. contents::
+
+Introduction
+============
+
+This package provides a new control panel for the Plone content management 
+system.  The purpose of this control panel is to allow easy replacement of the
+site logo.  
+
+After installing the package, the user will find a new control panel listed
+under the products category in Plone's Site Setup area. Clicking on the name
+of the panel will open a view providing a preview of the current site logo and
+a file upload field to replace that logo. If the user chooses to upload a new
+site logo, that new logo will replace the existing one, and the logo will
+change for the site.
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '1.0a1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='eastofeaton.logopanel',
+      version=version,
+      description="A Plone control panel  to allow overriding the plone site logo",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone',
+      author='Cris Ewing',
+      author_email='cris@crisewing.com',
+      url='https://github.com/cewing/eastofeaton.logopanel',
+      license='gpl',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eastofeaton'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      )
