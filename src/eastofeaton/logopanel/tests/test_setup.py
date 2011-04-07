0ec5fc34a3613c17276126908954cbc94415ef94
cewing <cris@crisewing.com>
2011-04-06 23:34:25 -0700

    adding a basic, failing test to make sure the wiring is all right

--- src/eastofeaton/logopanel/tests/test_setup.py
+++ src/eastofeaton/logopanel/tests/test_setup.py
@@ -6,3 +6,15 @@ from plone.app.testing import setRoles
 
 from eastofeaton.logopanel.testing import EASTOFEATON_LOGOPANEL_INTEGRATION_TESTING
 
+
+class TestSetup(unittest.TestCase):
+
+    layer = EASTOFEATON_LOGOPANEL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+
+    def test_foo(self):
+        import pdb; pdb.set_trace()
+        self.assertTrue(False, 'this test will fail, right?')
