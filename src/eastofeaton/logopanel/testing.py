2f5e5069f0f89a30ff80c4009d986900357b07e2
cewing <cris@crisewing.com>
2011-04-06 23:25:31 -0700

    adding test harness with plone.app.testing.  This shit ROCKS!!!!

---
+++ src/eastofeaton/logopanel/testing.py
@@ -0,0 +1,31 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+
+from zope.configuration import xmlconfig
+
+
+class EastofeatonLogoPanel(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+
+        # Load ZCML
+        import eastofeaton.logopanel
+        xmlconfig.file('configure.zcml',
+                       eastofeaton.logopanel,
+                       context=configurationContext)
+
+        def setUpPloneSite(self, portal):
+            applyProfile(portal, 'eastofeaton.logopanel:default')
+
+EASTOFEATON_LOGOPANEL_FIXTURE = EastofeatonLogoPanel()
+EASTOFEATON_LOGOPANEL_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(EASTOFEATON_LOGOPANEL_FIXTURE, ),
+                       name="EastofeatonLogoPanel:Integration")
+EASTOFEATON_LOGOPANEL_FUNCTIONAL_TESTING = \
+    FunctionalTesting(bases=(EASTOFEATON_LOGOPANEL_FIXTURE, ),
+                      name="EastofeatonLogoPanel:Functional")
---
+++ src/eastofeaton/logopanel/tests/test_setup.py
@@ -0,0 +1,8 @@
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_ID, TEST_USER_PASSWORD
+from plone.app.testing import applyProfile
+from plone.app.testing import setRoles
+
+from eastofeaton.logopanel.testing import EASTOFEATON_LOGOPANEL_INTEGRATION_TESTING
+
