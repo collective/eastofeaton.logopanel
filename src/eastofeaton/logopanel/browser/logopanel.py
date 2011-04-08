dc4c1e6310f1132c66ce23a543cbf25892de9d18
cewing <cris@crisewing.com>
2011-04-07 19:10:49 -0700

    complete logic for the control panel form _on_save method.  Add a custom template.  We will want to undo this and do it the right way, with a custom widget, but for now it will do.

--- src/eastofeaton/logopanel/browser/logopanel.py
+++ src/eastofeaton/logopanel/browser/logopanel.py
@@ -1,4 +1,5 @@
 from zope.schema import Bytes
+from zope.schema import ValidationError
 from zope.interface import Interface
 from zope.interface import implements
 from zope.component import adapts
@@ -6,16 +7,34 @@ from zope.formlib import form
 from zope.i18nmessageid import MessageFactory
 from plone.app.controlpanel.form import ControlPanelForm
 
+from OFS.Image import Image
+from OFS.Image import getImageInfo
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.CMFCore.utils import getToolByName
 
 _ = MessageFactory('LogoPanel')
+
+LOGO_ID = 'logo.png'
+
+
+class ImageValidationError(ValidationError):
+    """Supplied file does not appear to be an image"""
+
+
+def validateLogoIsImage(value):
+    ct, w, h = getImageInfo(value)
+    if ct == '':
+        raise ImageValidationError(value)
+    return True
 
 
 class ILogoPanelSchema(Interface):
 
     logo = Bytes(title=_(u'New Logo File'),
                  description=_(u'Upload a new file to replace the site logo'),
-                 required=False)
+                 required=True,
+                 constraint=validateLogoIsImage)
 
 
 class LogoPanelAdapter(object):
@@ -27,23 +46,77 @@ class LogoPanelAdapter(object):
         self.context = context
 
     def get_logo(self):
-        print 'I am the getter method'
-        import pdb; pdb.set_trace( )
-        return ''
+        logo = ''
+        try:
+            logo = self.context.restrictedTraverse(LOGO_ID)
+        except KeyError:
+            # there is no logo.png that can be traversed to, punt
+            pass
+
+        return logo
 
     def set_logo(self, value):
-        import pdb; pdb.set_trace( )
-        print 'I am the setter method'
+        # it just seems better to do this in the 'on save' method below
+        pass
 
     logo = property(get_logo, set_logo)
 
 
 class LogoPanel(ControlPanelForm):
+
+    template = ViewPageTemplateFile('logopanel.pt')
+
     form_fields = form.FormFields(ILogoPanelSchema)
-    form_name = _(u'Site Logo Control Panel')
+    form_name = _(u'Logo Upload Form')
     label = _(u'Site Logo Control Panel')
     description = _(u'Replace the site-wide logo')
 
-    def _on_save(self):
-        print('I am the _on_save method')
-        import pdb; pdb.set_trace( )
+    @property
+    def current_logo(self):
+        tag = '<span class="discreet"><No Logo Found></span>'
+        try:
+            logo = self.context.restrictedTraverse(LOGO_ID)
+            tag = logo.tag()
+        except KeyError:
+            # none was found, return the default tag
+            pass
+
+        return tag
+
+    def _on_save(self, data=None):
+        if data is None:
+            # no form data, return without doing anything
+            return
+
+        new_logo = data['logo']
+        if not new_logo:
+            # logo not uploaded, return without doing anything
+            return
+
+        skins = getToolByName(self.context, 'portal_skins')
+        target = self.context
+        if 'custom' in skins:
+            target = skins['custom']
+
+        if LOGO_ID in target:
+            img = target[LOGO_ID]
+            if isinstance(img, Image):
+                # this is an OFS image, or subclass, we should have the
+                # update_data method
+                try:
+                    img.update_data(new_logo)
+                    return
+                except TypeError, e:
+                    # there is a problem with the logo data, it's unicode
+                    raise e
+                except AttributeError, e:
+                    # no update_data method.  make a new one and replace this
+                    # one
+                    pass
+            else:
+                # let's override it, it isn't an expected type.
+                pass
+
+        img = Image(LOGO_ID, 'Custom Site Logo', new_logo)
+        target._setObject(LOGO_ID, img)
+        return
